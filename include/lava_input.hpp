ed59d0c63b2fcec9b2d99dae1245944b0fec8ad5
Dany <riverodi@esat-alumni.com>
2024-10-22 20:31:15 +0200

    lava input almost finish

--- include/lava_input.hpp
+++ include/lava_input.hpp
@@ -19,77 +19,267 @@ class LavaInput {
 
 public:
 
+	/**
+	* @brief Default constructor
+	*
+	* @param window the window to link this input.
+	*/
 	LavaInput(GLFWwindow* window);
 
+	/**
+	* @brief Default destructor
+	*
+	*/
 	~LavaInput();
 
-	// Keys & Mouse Implementation
+	/****************** Keys & Mouse Implementation **************/
 
+	/**
+	* @brief Check the first frame the key is being press. (ONLY FOR KEYBOARD AND MOUSE)
+	* 
+	* @param key the key to be check
+	*/
 	bool isInputPressed(int key);
 
+	/**
+	* @brief Check the first frame the key is releases. (ONLY FOR KEYBOARD AND MOUSE)
+	*
+	* @param key the key to be check
+	*/
 	bool isInputReleased(int key);
 
+	/**
+	* @brief Check if the key is not being press. (ONLY FOR KEYBOARD AND MOUSE)
+	*
+	* @param key the key to be check
+	*/
 	bool isInputUp(int key);
 
+	/**
+	* @brief Check if the key is being press. (ONLY FOR KEYBOARD AND MOUSE)
+	*
+	* @param key the key to be check
+	*/
 	bool isInputDown(int key);
 
+	/**
+	* @brief Bind the recieve action to a key from mouse or keyboard. (ONLY FOR KEYBOARD AND MOUSE)
+	*
+	* @param action the action to bind(Should be an enum define by the user)
+	* @param key the key to be bound
+	*/
 	void BindActionToInput(int action, int key);
 
-	// Gamepad Implementation
+
+
+	/****************** Gamepad Implementation **************/
+
+	/**
+	* @brief Check the first frame the button, in the specified gamepad, is being press. (ONLY FOR GAMEPAD)
+	*
+	* @param game_pad the gamepad to be check
+	* @param button the button to be check
+	*/
 	bool isGamePadButtonPressed(int game_pad, int button);
 
+	/**
+	* @brief Check the first frame the button, in the specified gamepad, is released. (ONLY FOR GAMEPAD)
+	*
+	* @param game_pad the gamepad to be check
+	* @param button the button to be check
+	*/
 	bool isGamePadButtonReleased(int game_pad, int button);
 
+	/**
+	* @brief Check if the button, in the specified gamepad, is not being press. (ONLY FOR GAMEPAD)
+	*
+	* @param game_pad the gamepad to be check
+	* @param button the button to be check
+	*/
 	bool isGamePadButtonUp(int game_pad, int button);
 
+	/**
+	* @brief Check if the button, in the specified gamepad, is being press. (ONLY FOR GAMEPAD)
+	*
+	* @param game_pad the gamepad to be check
+	* @param button the button to be check
+	*/
 	bool isGamePadButtonDown(int game_pad, int button);
 
+	/**
+	* @brief Return the value of the axis in the specified gamepad, value between -1.0 and 1.0. (RANGE: [-1.0f, 1.0f]) (ONLY FOR GAMEPAD)
+	*
+	* @param game_pad the gamepad to be check
+	* @param button the button to be check
+	*/
 	float getGamePadAxis(int game_pad, int axis);
 
+	/**
+	* @brief Bind the recieve action to a button from the specified gamepad. (ONLY FOR GAMEPAD)
+	*
+	* @param action the action to bind(Should be an enum define by the user)
+	* @param game_pad the gamepad to be bound
+	* @param button the button to be bound
+	*/
 	void BindActionToGamePadButton(int action, int game_pad, int button);
 
+	/**
+	* @brief Bind the recieve action to an axis from the specified gamepad, an action can only be bound to one axis. (ONLY FOR GAMEPAD)
+	*
+	* @param action the action to bind(Should be an enum define by the user)
+	* @param game_pad the gamepad to be bound
+	* @param axis the axis to be bound
+	*/
 	void BindActionToGamePadAxis(int action, int game_pad, int axis);
 
-	// Action Implementation
 
+
+	/****************** Action Implementation **************/
+
+	/**
+	* @brief Check the first frame the action is being press in all it's binds(keyboard, mouse or gamepad),
+	*		 return true if at least one of them is being press for the first time. (ONLY FOR ALREADY BIND ACTIONS)
+	*
+	* @param action the action to be check(Should be an enum define by the user)
+	*/
 	bool isActionPressed(int action);
 
+	/**
+	* @brief Check the first frame the action is released in all it's binds(keyboard, mouse or gamepad),
+	*		 return true if at least one of them is being release this frame. (ONLY FOR ALREADY BIND ACTIONS)
+	*
+	* @param action the action to be check(Should be an enum define by the user)
+	*/
 	bool isActionReleased(int action);
 
+	/**
+	* @brief Check if the action is not press in all it's binds(keyboard, mouse or gamepad),
+	*		 return true if all of them are not being press. (ONLY FOR ALREADY BIND ACTIONS)
+	*
+	* @param action the action to be check(Should be an enum define by the user)
+	*/
 	bool isActionUp(int action);
 
+	/**
+	* @brief Check if the action is press in all it's binds(keyboard, mouse or gamepad),
+	*		 return true if at least one of them is being press. (ONLY FOR ALREADY BIND ACTIONS)
+	*
+	* @param action the action to be check(Should be an enum define by the user)
+	*/
 	bool isActionDown(int action);
 
+	/**
+	* @brief Return the value of the action, value between -1.0 and 1.0. (RANGE: [-1.0f, 1.0f]) (ONLY FOR ALREADY BIND ACTIONS)
+	*
+	* @param action the action to be check(Should be an enum define by the user)
+	*/
 	float getActionAxis(int action);
 
 private:
 
 	LavaInput() = delete;
 
+	/**
+	* @var GLFWwindow window_
+	* @brief Stores the glfw window link to the input.
+	*/
 	GLFWwindow* window_;
 
-	// Keys & Mouse Implementation
-	std::unordered_map<int, KeyProperties> kb_mouse_input_properties_map;
-	std::map<int, std::set<int>> kb_mouse_action_map;
-	void kb_mouse_callback(int key, int scancode, int action, int mods);
-
-	// GamePad Implementation
-	std::map<int, GamepadState> game_pad_states;
-	std::map<int, std::vector<GamePadAction>> gamepad_action_map;
-	std::map<int, GamePadAction> gamepad_axis_action_map;
-
+	/**
+	* @brief Process the value of all keys and collect the state of all use gamepads.
+	*/
 	void ProcessEndFrame();
 
-	//Static Properties and Functions
 
-	static std::unordered_map<GLFWwindow*, LavaInput*> input_map;
+	/****************** Keys & Mouse Implementation **************/
 
+	/**
+	* @var std::unordered_map<int, KeyProperties> kb_mouse_input_properties_map
+	* @brief Stores the input an its properties. (ONLY FOR KEYBOARD AND MOUSE)
+	*/
+	std::unordered_map<int, KeyProperties> kb_mouse_input_properties_map_;
+
+	/**
+	* @var std::map<int, std::set<int>> kb_mouse_action_map
+	* @brief Stores the actions an it's binds keys. (ONLY FOR KEYBOARD AND MOUSE)
+	*/
+	std::map<int, std::set<int>> kb_mouse_action_map_;
+
+	/**
+	* @brief Assign the properties of the recive key, checking the action value.
+	*
+	* @param key The key to recive the action
+	* @param input_type Mainly use the diferentiate the type of input(-1 == mouse), if is keyboard then is the system-specific scancode of the key
+	* @param action Recive the action(GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT)
+	* @param mods Bit field describing which modifier keys were held down.
+	*/
+	void kb_mouse_callback(int key, int input_type, int action, int mods);
+
+
+	/****************** Gamepad Implementation **************/
+
+	/**
+	* @var std::map<int, GamepadState> game_pad_states
+	* @brief Stores the input an its properties. (ONLY FOR GAMEPAD)
+	*/
+	std::map<int, GamepadState> game_pad_states_;
+
+	/**
+	* @var std::map<int, std::vector<GamePadAction>> gamepad_action_map
+	* @brief Stores the actions an it's binds buttons. (ONLY FOR GAMEPAD)
+	*/
+	std::map<int, std::vector<GamePadAction>> gamepad_action_map_;
+
+	/**
+	* @var std::map<int, std::vector<GamePadAction>> gamepad_action_map
+	* @brief Stores the actions an it's binds axis. (ONLY FOR GAMEPAD)
+	*/
+	std::map<int, GamePadAction> gamepad_axis_action_map_;
+
+
+	/****************** Static Properties and Functions **************/
+
+	/**
+	* @var static std::unordered_map<GLFWwindow*, LavaInput*> input_map
+	* @brief Stores the Window and the input associated to it.
+	*/
+	static std::unordered_map<GLFWwindow*, LavaInput*> s_input_map_;
+
+	/**
+	* @brief Call the specific input associated to the recive window
+	*		 assigning the properties of the recive key, checking the action value. (FOR KEYBOARD)
+	*
+	* @param window The window to call
+	* @param key The key to recive the action
+	* @param scancode The system-specific scancode of the key
+	* @param action Recive the action(GLFW_PRESS, GLFW_RELEASE or GLFW_REPEAT)
+	* @param mods Bit field describing which modifier keys were held down.
+	*/
 	static void global_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
+	/**
+	* @brief Call the specific input associated to the recive window
+	*		 assigning the properties of the recive key, checking the action value. (FOR MOUSE)
+	*
+	* @param window The window to call
+	* @param button The button to recive the action
+	* @param action Recive the action(GLFW_PRESS or GLFW_RELEASE)
+	* @param mods Bit field describing which modifier keys were held down.
+	*/
 	static void global_mouse_callback(GLFWwindow* window, int button, int action, int mods);
 
+	/**
+	* @brief Clear the specific input associated to the recive window
+	*
+	* @param window The window to clear
+	*/
 	static void clean_bindings(GLFWwindow* window);
 
+	/**
+	* @brief Process the end frame, for the input, in all the windows in input_map
+	*
+	* @param window The window to clear
+	*/
 	static void end_frame();
 };
 
--- src/lava_input.cpp
+++ src/lava_input.cpp
@@ -13,53 +13,59 @@
 #include "lava_input.hpp"
 #include "lava_engine.hpp"
 
-std::unordered_map<GLFWwindow*, LavaInput*> LavaInput::input_map;
+std::unordered_map<GLFWwindow*, LavaInput*> LavaInput::s_input_map_;
 
 LavaInput::LavaInput(GLFWwindow* window) : window_{window}
 {
+	//Set all callbacks to the window
 	glfwSetKeyCallback(window_, global_key_callback);
 	glfwSetMouseButtonCallback(window_, global_mouse_callback);
 	glfwSetWindowCloseCallback(window_, clean_bindings);
 
-	if(input_map.empty()) LavaEngine::end_frame_callbacks.push_back(end_frame);
+	//If this is the first window add the end frame callback to the engine
+	if(s_input_map_.empty()) LavaEngine::end_frame_callbacks.push_back(end_frame);
 
-	input_map.emplace(window_, this);
+	//Add this window to the map
+	s_input_map_.emplace(window_, this);
 }
 
 LavaInput::~LavaInput()
 {
+	//Clear all bindings to the window
 	clean_bindings(window_);
 }
 
 bool LavaInput::isInputPressed(int key)
 {
-	return kb_mouse_input_properties_map[key].past_frame_properties & KEY_PRESS;
+	return kb_mouse_input_properties_map_[key].past_frame_properties & KEY_PRESS;
 }
 
 bool LavaInput::isInputReleased(int key)
 {
-	return kb_mouse_input_properties_map[key].past_frame_properties & KEY_RELEASE;
+	return kb_mouse_input_properties_map_[key].past_frame_properties & KEY_RELEASE;
 }
 
 bool LavaInput::isInputUp(int key)
 {
-	return kb_mouse_input_properties_map[key].past_frame_properties == 0 ||
-		kb_mouse_input_properties_map[key].past_frame_properties & KEY_RELEASE;
+	return kb_mouse_input_properties_map_[key].past_frame_properties == 0 ||
+		kb_mouse_input_properties_map_[key].past_frame_properties & KEY_RELEASE;
 }
 
 bool LavaInput::isInputDown(int key)
 {
-	return kb_mouse_input_properties_map[key].past_frame_properties & KEY_PRESS ||
-		kb_mouse_input_properties_map[key].past_frame_properties & KEY_REPEAT;
+	return kb_mouse_input_properties_map_[key].past_frame_properties & KEY_PRESS ||
+		kb_mouse_input_properties_map_[key].past_frame_properties & KEY_REPEAT;
 }
 
 bool LavaInput::isActionPressed(int action)
 {
-	for (int i : kb_mouse_action_map[action]) {
+	//Check all input(keyboard and mouse) if one is true return true
+	for (int i : kb_mouse_action_map_[action]) {
 		if (isInputPressed(i)) return true;
 	}
 
-	for (GamePadAction i : gamepad_action_map[action]) {
+	//Check all input(gamepad) if one is true return true
+	for (GamePadAction i : gamepad_action_map_[action]) {
 		if (isGamePadButtonPressed(i.game_pad, i.game_pad_button)) return true;
 	}
 
@@ -68,11 +74,12 @@ bool LavaInput::isActionPressed(int action)
 
 bool LavaInput::isActionReleased(int action)
 {
-	for (int i : kb_mouse_action_map[action]) {
+	//Check all input(keyboard and mouse) if one is true return true
+	for (int i : kb_mouse_action_map_[action]) {
 		if (isInputReleased(i)) return true;
 	}
-
-	for (GamePadAction i : gamepad_action_map[action]) {
+	//Check all input(gamepad) if one is true return true
+	for (GamePadAction i : gamepad_action_map_[action]) {
 		if (isGamePadButtonReleased(i.game_pad, i.game_pad_button)) return true;
 	}
 
@@ -81,24 +88,28 @@ bool LavaInput::isActionReleased(int action)
 
 bool LavaInput::isActionUp(int action)
 {
-	for (int i : kb_mouse_action_map[action]) {
-		if (isInputUp(i)) return true;
+	//Check all input(keyboard and mouse) if one is false return false(at least one is down)
+	for (int i : kb_mouse_action_map_[action]) {
+		if (isInputDown(i)) return false;
 	}
 
-	for (GamePadAction i : gamepad_action_map[action]) {
-		if (isGamePadButtonUp(i.game_pad, i.game_pad_button)) return true;
+	//Check all input(gamepad) if one is false return false(at least one is down)
+	for (GamePadAction i : gamepad_action_map_[action]) {
+		if (isGamePadButtonDown(i.game_pad, i.game_pad_button)) return false;
 	}
 
-	return false;
+	return true;
 }
 
 bool LavaInput::isActionDown(int action)
 {
-	for (int i : kb_mouse_action_map[action]) {
+	//Check all input(keyboard and mouse) if one is true return true(at least one is down)
+	for (int i : kb_mouse_action_map_[action]) {
 		if (isInputDown(i)) return true;
 	}
 
-	for (GamePadAction i : gamepad_action_map[action]) {
+	//Check all input(gamepad) if one is true return true(at least one is down)
+	for (GamePadAction i : gamepad_action_map_[action]) {
 		if (isGamePadButtonDown(i.game_pad, i.game_pad_button)) return true;
 	}
 
@@ -107,85 +118,92 @@ bool LavaInput::isActionDown(int action)
 
 float LavaInput::getActionAxis(int action)
 {
-	return getGamePadAxis(gamepad_axis_action_map[action].game_pad, gamepad_axis_action_map[action].game_pad_button);
+	return getGamePadAxis(gamepad_axis_action_map_[action].game_pad, gamepad_axis_action_map_[action].game_pad_button);
 }
 
 void LavaInput::BindActionToInput(int action, int key)
 {
 	//Find or Create the new Action
-	kb_mouse_action_map[action].insert(key);
+	kb_mouse_action_map_[action].insert(key);
 }
 
 bool LavaInput::isGamePadButtonPressed(int game_pad, int button)
 {
-	return game_pad_states[game_pad].state.buttons[button] & KEY_PRESS && 
-		   game_pad_states[game_pad].is_active;
+	return game_pad_states_[game_pad].state.buttons[button] & KEY_PRESS &&
+		game_pad_states_[game_pad].is_active;
 }
 
 bool LavaInput::isGamePadButtonReleased(int game_pad, int button)
 {
-	return game_pad_states[game_pad].state.buttons[button] & KEY_RELEASE &&
-		   game_pad_states[game_pad].is_active;
+	return game_pad_states_[game_pad].state.buttons[button] & KEY_RELEASE &&
+		game_pad_states_[game_pad].is_active;
 }
 
 bool LavaInput::isGamePadButtonUp(int game_pad, int button)
 {
-	return (game_pad_states[game_pad].state.buttons[button] == 0 ||
-		    game_pad_states[game_pad].state.buttons[button] & KEY_RELEASE) &&
-		    game_pad_states[game_pad].is_active;
+	return (game_pad_states_[game_pad].state.buttons[button] == 0 ||
+		game_pad_states_[game_pad].state.buttons[button] & KEY_RELEASE) &&
+		game_pad_states_[game_pad].is_active;
 }
 
 bool LavaInput::isGamePadButtonDown(int game_pad, int button)
 {
-	return (game_pad_states[game_pad].state.buttons[button] & KEY_PRESS ||
-		    game_pad_states[game_pad].state.buttons[button] & KEY_REPEAT) &&
-		    game_pad_states[game_pad].is_active;
+	return (game_pad_states_[game_pad].state.buttons[button] & KEY_PRESS ||
+		game_pad_states_[game_pad].state.buttons[button] & KEY_REPEAT) &&
+		game_pad_states_[game_pad].is_active;
 }
 
 float LavaInput::getGamePadAxis(int game_pad, int axis)
 {
-	if (!game_pad_states[game_pad].is_active) return 0;
-	return game_pad_states[game_pad].state.axes[axis];
+	if (!game_pad_states_[game_pad].is_active) return 0;
+	return game_pad_states_[game_pad].state.axes[axis];
 }
 
 void LavaInput::BindActionToGamePadButton(int action, int game_pad, int button)
 {
 	//Find or Create the new Action
-	gamepad_action_map[action].push_back(GamePadAction(game_pad, button));
+	gamepad_action_map_[action].push_back(GamePadAction(game_pad, button));
 }
 
 void LavaInput::BindActionToGamePadAxis(int action, int game_pad, int axis)
 {
 	//Find or Create the new Action
-	gamepad_axis_action_map[action].game_pad = game_pad;
-	gamepad_axis_action_map[action].game_pad_button = axis;
+	gamepad_axis_action_map_[action].game_pad = game_pad;
+	gamepad_axis_action_map_[action].game_pad_button = axis;
 }
 
-void LavaInput::kb_mouse_callback(int key, int scancode, int action, int mods)
+void LavaInput::kb_mouse_callback(int key, int input_type, int action, int mods)
 {
 	int32_t aux_current_frame_properties = 0;
-	if (action & GLFW_PRESS) {
-		aux_current_frame_properties = aux_current_frame_properties | KEY_PRESS;
-		if(scancode == -1) aux_current_frame_properties = aux_current_frame_properties | SUSTAIN_TIL_RELEASE;
+	if (action & GLFW_PRESS) { //If is press this frame
+		aux_current_frame_properties = aux_current_frame_properties | KEY_PRESS; //Add Press property
+		//If input type is mouse add Sustain Til Release Property
+		if(input_type == -1) aux_current_frame_properties = aux_current_frame_properties | SUSTAIN_TIL_RELEASE;
 	}
-	else if (action & GLFW_REPEAT) aux_current_frame_properties = aux_current_frame_properties | KEY_REPEAT;
-	else aux_current_frame_properties = aux_current_frame_properties | KEY_RELEASE;
-	//Find or Create the new key
-	kb_mouse_input_properties_map[key].current_frame_properties = aux_current_frame_properties;
-	kb_mouse_input_properties_map[key].last_action_time = glfwGetTime();
+	else if (action & GLFW_REPEAT) aux_current_frame_properties = aux_current_frame_properties | KEY_REPEAT; //If is repeated this frame add Repeated property
+	else aux_current_frame_properties = aux_current_frame_properties | KEY_RELEASE; //If not Press or Repeated then is Release, add Release property
+	//Find or Create the new key and set current frame property
+	kb_mouse_input_properties_map_[key].current_frame_properties = aux_current_frame_properties;
+	//Set current time(not currently use)
+	kb_mouse_input_properties_map_[key].last_action_time = glfwGetTime();
 }
 
 void LavaInput::ProcessEndFrame()
 {
-	for(auto i = kb_mouse_input_properties_map.begin(); i != kb_mouse_input_properties_map.end(); i++) {
+	//Process al keys in kb_mouse_input_properties_map_(keyboard and mouse)
+	for (auto i = kb_mouse_input_properties_map_.begin(); i != kb_mouse_input_properties_map_.end(); i++) {
+		//Assign the data collected this frame in the past frame
 		i->second.past_frame_properties = i->second.current_frame_properties;
+		// If posses Sustanin Til Release Property assing Sustain Til Release and Repeat property to the new frame property
 		if (i->second.current_frame_properties & SUSTAIN_TIL_RELEASE) i->second.current_frame_properties = SUSTAIN_TIL_RELEASE | KEY_REPEAT;
-		else i->second.current_frame_properties = 0;
+		else i->second.current_frame_properties = 0; // Else assign no properties
 	}
 
-	for (auto i = game_pad_states.begin(); i != game_pad_states.end(); i++) {
+	//Process al gamepad states in game_pad_states_(only gamepads)
+	for (auto i = game_pad_states_.begin(); i != game_pad_states_.end(); i++) {
+		//Save past frame state for later checking
 		GLFWgamepadstate aux = i->second.state;
-		if (glfwGetGamepadState(i->first, &i->second.state) == GLFW_TRUE) {
+		if (glfwGetGamepadState(i->first, &i->second.state) == GLFW_TRUE) { // If gamepad is connected process the state
 			i->second.is_active = true;
 			for (int j = 0; j < 15; j++) {
 				if (aux.buttons[j] == GLFW_RELEASE && i->second.state.buttons[j] == GLFW_PRESS) {
@@ -197,29 +215,29 @@ void LavaInput::ProcessEndFrame()
 				}
 			}
 		}
-		else i->second.is_active = false;
+		else i->second.is_active = false;  // If gamepad is not conected set is active to fasle
 	}
 }
 
 void LavaInput::global_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-	input_map[window]->kb_mouse_callback(key, scancode, action, mods);
+	s_input_map_[window]->kb_mouse_callback(key, scancode, action, mods);
 }
 
 void LavaInput::global_mouse_callback(GLFWwindow* window, int button, int action, int mods)
 {
-	input_map[window]->kb_mouse_callback(button, -1, action, mods);
+	s_input_map_[window]->kb_mouse_callback(button, -1, action, mods);
 }
 
 void LavaInput::clean_bindings(GLFWwindow* window)
 {
 	glfwSetKeyCallback(window, nullptr);
-	input_map.erase(window);
+	s_input_map_.erase(window);
 }
 
 void LavaInput::end_frame()
 {
-	for (std::pair<GLFWwindow*, LavaInput*> input_pair : input_map) {
+	for (std::pair<GLFWwindow*, LavaInput*> input_pair : s_input_map_) {
 		input_pair.second->ProcessEndFrame();
 	}
 }
